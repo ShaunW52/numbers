d2d70958f5656354ce8609e564e4cbd1d5b3df37
Shaun Wiechmann <wiechmann@chapman.edu>
2018-11-09 11:46:41 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#NUMBERS.sh
+#Shaun Wiechmann
+echo "Enter a positive number: "
+read NUMBERS
+N=1
+while echo $NUMBERS |  $N -le $NUMBERS
+if
+	$((N%2)) -eq 0
+then
+	echo $N "Even"
+else
+	echo $N "Odd"
+N=$((N+1))
+fi
